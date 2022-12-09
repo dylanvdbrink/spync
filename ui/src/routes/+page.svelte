3d0a15110c540705001e113f981033eb891066a0
Dylan van den Brink <dylanvandenbrink@live.nl>
2022-12-09 20:05:30 +0100

    Added tailwind

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+
+<div class="bg-slate-700 h-full">
+    <header class="bg-slate-800">
+        <nav class="border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                <a href="#" class="flex items-center">
+                    <img src="logo.png" class="mr-3 h-6 sm:h-9" alt="Logo" />
+                </a>
+            </div>
+        </nav>
+    </header>
+</div>
