f9d27955d7791c15300c7c9f28c9a5e4e2d9cf1f
Dylan van den Brink <dylanvandenbrink@live.nl>
2022-12-10 17:50:27 +0100

    Added working spotify authentication and first controllers/logic

--- api/cmd/spync-api/main.go
+++ api/cmd/spync-api/main.go
@@ -20,6 +20,11 @@ func main() {
 	router.GET("/settings", controllers.GetSettings)
 	router.POST("/settings", controllers.SaveSettings)
 
+	router.GET("/spotify/auth", controllers.GetAuthURL)
+	router.GET("/spotify/save-auth", controllers.SaveAuth)
+	router.GET("/spotify/me", controllers.GetMe)
+	router.GET("/spotify/me/playlists", controllers.GetPlaylists)
+
 	err = router.Run()
 	if err != nil {
 		fmt.Println(err.Error())
--- api/internal/configuration/configuration.go
+++ api/internal/configuration/configuration.go
@@ -4,15 +4,27 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"time"
 )
 
 type SpotifyConfig struct {
+	AccessToken  string    `json:"access-token"`
+	RefreshToken string    `json:"refresh-token"`
+	TokenType    string    `json:"token-type"`
+	Expiry       time.Time `json:"expiry"`
+
+	PlaylistIds []string `json:"playlist-ids"`
+
 	ClientId     string `json:"client-id"`
 	ClientSecret string `json:"client-secret"`
+	RedirectUrl  string `json:"redirect-url"`
 }
 
 type AppleMusicConfig struct {
+	AccessToken  string `json:"access-token"`
+	RefreshToken string `json:"refresh-token"`
 	ClientId     string `json:"client-id"`
 	ClientSecret string `json:"client-secret"`
 }
@@ -27,27 +39,25 @@ type Config struct {
 var defaultConfig = Config{
 	Syncing:      false,
 	SyncInterval: 4320, // 3 days
-	AppleMusic:   AppleMusicConfig{ClientId: "", ClientSecret: ""},
-	Spotify:      SpotifyConfig{ClientId: "", ClientSecret: ""},
+	AppleMusic:   AppleMusicConfig{},
+	Spotify:      SpotifyConfig{},
 }
 
 const configFilePath = "configuration.json"
 
 func GetConfiguration() (Config, error) {
-	var config Config
-
 	file, fileErr := getOrCreateConfigurationFile()
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
 	if fileErr != nil {
 		fmt.Println("error getting file: " + fileErr.Error())
 		return Config{}, fileErr
 	}
 
-	decoder := json.NewDecoder(file)
-	decodeErr := decoder.Decode(&config)
-
-	if decodeErr != nil {
-		return Config{}, decodeErr
-	}
+	configContentBytes, _ := io.ReadAll(file)
+	var config Config
+	_ = json.Unmarshal(configContentBytes, &config)
 
 	return config, nil
 }
@@ -62,12 +72,13 @@ func getOrCreateConfigurationFile() (*os.File, error) {
 
 		err := writeConfiguration(newFile, defaultConfig)
 		if err != nil {
+			fmt.Println("writeConfiguration error: " + err.Error())
 			return nil, err
 		}
 
 		return newFile, nil
 	} else {
-		file, openErr := os.OpenFile(configFilePath, os.O_RDWR, 0644)
+		file, openErr := os.OpenFile(configFilePath, os.O_RDWR, 0755)
 		if openErr != nil {
 			fmt.Println("error opening file: " + openErr.Error())
 			return nil, openErr
@@ -77,21 +88,25 @@ func getOrCreateConfigurationFile() (*os.File, error) {
 }
 
 func SaveConfiguration(config Config) error {
+	fmt.Println("> SaveConfiguration")
+	// Replace properties that should not be touched
+	replaceErr := replaceProtectedProperties(config)
+	if replaceErr != nil {
+		fmt.Println("error replacing properties: " + replaceErr.Error())
+		return replaceErr
+	}
+
 	file, fileErr := getOrCreateConfigurationFile()
 	defer func(file *os.File) {
-		_ = file.Close()
+		closeErr := file.Close()
+		if closeErr != nil {
+			fmt.Println("error closing file: " + closeErr.Error())
+		}
 	}(file)
 	if fileErr != nil {
 		fmt.Println("could not get or create configfile: " + fileErr.Error())
 		return fileErr
 	}
-
-	// Replace properties that should not be touched
-	existingConfig, getErr := GetConfiguration()
-	if getErr != nil {
-		return getErr
-	}
-	config.Syncing = existingConfig.Syncing
 
 	err := writeConfiguration(file, config)
 	if err != nil {
@@ -99,15 +114,29 @@ func SaveConfiguration(config Config) error {
 		return err
 	}
 
+	fmt.Println("< SaveConfiguration")
+
+	return nil
+}
+
+func replaceProtectedProperties(config Config) error {
+	existingConfig, getErr := GetConfiguration()
+	if getErr != nil {
+		fmt.Println("error getting config: " + getErr.Error())
+		return getErr
+	}
+	config.Syncing = existingConfig.Syncing
+	config.Spotify.AccessToken = existingConfig.Spotify.AccessToken
+	config.Spotify.RefreshToken = existingConfig.Spotify.RefreshToken
+	config.Spotify.PlaylistIds = existingConfig.Spotify.PlaylistIds
+
 	return nil
 }
 
 func writeConfiguration(file *os.File, config Config) error {
-	encoder := json.NewEncoder(file)
-	err := encoder.Encode(config)
-	if err != nil {
-		fmt.Println("encoding error: " + err.Error())
-		return err
-	}
+	configBytes, _ := json.MarshalIndent(config, "", "  ")
+	_ = file.Truncate(0)
+	_, _ = file.Write(configBytes)
+
 	return nil
 }
